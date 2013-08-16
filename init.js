7a444dd2700f345815b7c8b6ce2de68c3d9c175c
ravik122 <ravik1222@gmail.com>
2013-08-16 18:39:20 +0200

    changed field init

--- init.js
+++ init.js
@@ -2,20 +2,19 @@ var fields = new Array();
 var mobs = [];
 function Field(number, x, y)
 {
-//	var number;
-//	var x;
-//	var y;
-//	var traversable = true;
-//	var ngbs = new Array();
-//	var distance = 999999;
-//	var parentt;
 	this.number = number;
+	//center coords
 	this.x = x;
 	this.y = y;
 	this.traversable = true;
 	this.ngbs = new Array();
 	this.distance = 99999;
 	this.parentt = null;
+
+	this.squaredCenterDistance = function( field2 )
+	{
+		return (x-field2.x)*(x-field2.x)+(y-field2.y)*(y-field2.y);
+	}
 }
 
 function Mob(x, y, field)
@@ -23,6 +22,8 @@ function Mob(x, y, field)
 	this.x = x;
 	this.y = y;
 	this.field = field;
+	this.field_distx = 0;
+	this.field_disty = 0;
 }
 
 function init_mobs( spawn_field )
@@ -35,34 +36,87 @@ function init_mobs( spawn_field )
 }
 
 
-function init()
+function init_fields()
 {
-	//populate fields:
-	// 12 x 12 fields 50px x 50px
-	for( var i = 0; i < 12; i ++ )
-	{
-		for( var j = 0; j < 12; j++ )
-		{
-			var field = new Field(12 * i + j, i * 50, j * 50);
-			fields.push( field );
-		}
-	}
-	for( j = 0; j < fields.length; j++ )
-	{
-		for( var k = 0; k < fields.length; k++ )
-		{
-			if( fields[j] !== fields[k] )
-			{
 
-				if( Math.abs( fields[j].x - fields[k].x ) <= 50 && 
-				    Math.abs( fields[j].y - fields[k].y ) <= 50 )
+	var hexHeight,
+		hexRadius,
+		hexRectangleHeight,
+		hexRectangleWidth,
+		hexagonAngle = 0.523598776, // 30 degrees in radians
+		sideLength = 20,
+		boardWidth = 17,
+		boardHeight = 19;
+
+	hexHeight = Math.sin(hexagonAngle) * sideLength;
+	hexRadius = Math.cos(hexagonAngle) * sideLength;
+	hexRectangleHeight = sideLength + 2 * hexHeight;
+	hexRectangleWidth = 2 * hexRadius;
+
+	var i,
+	    j;
+
+	for(i = 0; i < boardHeight; ++i) 
+	{
+	    for(j = 0; j < boardWidth; ++j) 
+	    {
+		    var x = j * hexRectangleWidth + ((i % 2) * hexRadius);
+		    var y = i * (sideLength + hexHeight);
+		var field = new Field( boardWidth * i + j, Math.round(x), Math.round(y + hexHeight / 2) );
+		fields.push( field );
+	    }
+	}
+	// use squared distance between centers of hexagons to determine neighbourhood
+	for( i = 0; i < fields.length; i++)
+	{
+		for( j = 0 ; j < fields.length; j++)
+		{
+			if( i != j )
+			{
+				//1225 is the distance between hexagons' centers of sideLength of 20
+				if( fields[i].squaredCenterDistance( fields[j] ) <= 1225 )
 				{
-					//sa sasiadami:
-					fields[j].ngbs.push( fields[k] );
+					fields[i].ngbs.push( fields[j] );
 				}
 			}
 		}
 	}
+
+
+
+
+}
+
+
+function init()
+{
+	//populate fields:
+	// 12 x 12 fields 50px x 50px
+	//for( var i = 0; i < 12; i ++ )
+	//{
+	//	for( var j = 0; j < 12; j++ )
+	//	{
+	//		var field = new Field(12 * i + j, i * 50, j * 50);
+	//		fields.push( field );
+	//	}
+	//}
+	//for( j = 0; j < fields.length; j++ )
+	//{
+	//	for( var k = 0; k < fields.length; k++ )
+	//	{
+	//		if( fields[j] !== fields[k] )
+	//		{
+        //
+	//			if( Math.abs( fields[j].x - fields[k].x ) <= 50 && 
+	//			    Math.abs( fields[j].y - fields[k].y ) <= 50 )
+	//			{
+	//				//sa sasiadami:
+	//				fields[j].ngbs.push( fields[k] );
+	//			}
+	//		}
+	//	}
+	//}
+	init_fields();
 	init_mobs( fields[ fields.length - 1 ] );
 }
 
--- moving.js
+++ moving.js
@@ -16,8 +16,19 @@ function move()
 		{
 			if( mobs[i].x != mobs[i].field.x && mobs[i].y != mobs[i].field.y )
 			{
-				mobs[i].x += 0.5;
-				mobs[i].y += 0.5;
+				mobs[i].x += mobs[i].field_distx;
+				mobs[i].y += mobs[i].field_disty;
+			}
+			else
+			{
+				var next_field = path.indexOf( mobs[i].field ) + 1;
+				if( next_field < path.length )
+				{
+					mobs[i].field = path[ next_field ];
+					mobs[i].field_distx = ( mobs[i].field.x -  mobs[i].x ) / 100;
+					mobs[i].field_disty = ( mobs[i].field.y -  mobs[i].y ) / 100;
+
+				}
 			}
 		}
 
@@ -27,6 +38,16 @@ function move()
 		context.clearRect(0, 0, canvas.width, canvas.height );
 
 		//draw
+		//for( var i = 0; i < fields.length; i++ )
+		//{
+		//	context.beginPath();
+		//	context.rect(fields[i].x, fields[i].y, 50, 50);
+		//	context.lineWidth = 2;
+		//	context.strokeStyle = 'black';
+		//	context.stroke();
+		//}
+		draw_hexboard( canvas, context );
+
 		for( var i = 0; i < mobs.length; i ++)
 		{
 			context.fillRect(mobs[i].x, mobs[i].y, 25, 12 );
