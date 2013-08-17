62279b98775b3235dc3a10f4900b451c5d656a3a
ravik122 <ravik1222@gmail.com>
2013-08-17 16:21:21 +0200

    path is now colored, event listener was added multiple times - moved to moving.js

--- hexagons.js
+++ hexagons.js
@@ -23,38 +23,6 @@ function draw_hexboard(canvas, ctx)
 
         drawBoard(ctx, boardWidth, boardHeight);
 
-        //canvas.addEventListener("click", function(eventInfo) {
-        //    var x,
-        //        y,
-        //        hexX,
-        //        hexY,
-        //        screenX,
-        //        screenY,
-        //        rect;
-        //
-        //    rect = canvas.getBoundingClientRect();
-        //
-        //    x = eventInfo.clientX - rect.left;
-        //    y = eventInfo.clientY - rect.top;
-        //
-        //    hexY = Math.floor(y / (hexHeight + sideLength));
-        //    hexX = Math.floor((x - (hexY % 2) * hexRadius) / hexRectangleWidth);
-        //
-        //    screenX = hexX * hexRectangleWidth + ((hexY % 2) * hexRadius);
-        //    screenY = hexY * (hexHeight + sideLength);
-        //
-        //    ctx.clearRect(0, 0, canvas.width, canvas.height);
-        //
-        //    drawBoard(ctx, boardWidth, boardHeight);
-        //
-        //    // Check if the mouse's coords are on the board
-        //    if(hexX >= 0 && hexX < boardWidth) {
-        //        if(hexY >= 0 && hexY < boardHeight) {
-        //            ctx.fillStyle = "#000000";
-        //            drawHexagon(ctx, screenX, screenY, true);
-        //        }
-        //    }
-        //});
     }
 
 
@@ -72,11 +40,13 @@ function drawBoard(canvasContext, width, height)
 		    var fill = false;
 		    var x = i * hexRectangleWidth + ((j % 2) * hexRadius);
 		    var y = j * (sideLength + hexHeight); 
+		    //coloring the path
 		    for( var k = 0; k < path.length; k++)
 		    {
-			    if( x == path[k].x && y == path[k].y - 5 )//5 is rounded hexHeight / 2
+			    if( Math.round(x) == path[k].x && Math.round(y) == path[k].y - 5 )//5 is rounded hexHeight / 2
 				    fill = true;
 		    }
+		    //----
                 drawHexagon( canvasContext, x, y, fill );
             }
         }
